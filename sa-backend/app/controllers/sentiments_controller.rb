d330f8af682694fb2c2fc31b294955a9789d3619
Mani M <manim@smartek21.com>
2018-11-27 22:21:50 +0530

    added dockerfile to deploy rails container

--- sa-backend/app/controllers/sentiments_controller.rb
+++ sa-backend/app/controllers/sentiments_controller.rb
@@ -4,8 +4,9 @@ require 'uri'
 class SentimentsController < ApplicationController
   # POST /sentiments
   def create
-  	body = JSON.parse(request.body.read)
-    req = Net::HTTP.post URI('http://0.0.0.0:5000/analyse/sentiment'),
+    body = JSON.parse(request.body.read)
+    p "SA_LOGIC_API_URL: #{ENV.fetch("SA_LOGIC_API_URL")}"
+    req = Net::HTTP.post URI("#{ENV.fetch("SA_LOGIC_API_URL")}/analyse/sentiment"),
                { 'sentence' => body['sentence'] }.to_json,
                "Content-Type" => "application/json"
     render json: JSON.parse(req.body)
